85274a2dc4d91cacbedbe15abdc3c0adad28b34b
mrsekut <k.cloudspider@gmail.com>
2025-04-20 11:19:20 +0900

    launch client app

--- client/src/frontend.tsx
+++ client/src/frontend.tsx
@@ -1,16 +1,35 @@
-/**
- * This file is the entry point for the React app, it sets up the root
- * element and renders the App component to the DOM.
- *
- * It is included in `src/index.html`.
- */
-
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import { ApolloProvider } from "@apollo/client/react";
+import { createHttpLink } from "@apollo/client/link/http";
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+  credentials: 'include'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+    query: {
+      fetchPolicy: 'network-only',
+    },
+  },
+  ssrMode: typeof window === 'undefined',
+});
 
 function start() {
   const root = createRoot(document.getElementById("root")!);
-  root.render(<App />);
+  root.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
 }
 
 if (document.readyState === "loading") {
